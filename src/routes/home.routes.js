04da04ec45b7c769034642bc66cc82550d34e936
gabrieladeproinf <gabriela.deproinf@gmail.com>
2024-12-11 10:39:03 -0400

    asd2

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -7,6 +7,6 @@ router.get('/getServices', Home.getServicios);
 router.post('/contactService', Home.saveContactService)
 router.get('/getContactService', Home.getServicesContact)
 router.post('/getServicesByCategory', Home.getServicesCategories)
-router.post('/getSubscriptionByI', Home.getSubscriptionsById)
+router.post('/getSubscriptionById', Home.getSubscriptionsById)
 
 module.exports = router;
